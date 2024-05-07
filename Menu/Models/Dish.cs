8b609f41def593b8f82c07d4be7f5b1d331187d1
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-05-07 18:06:40 +0100

    Add project files.

---
+++ Menu/Models/Dish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Menu.Models;
+namespace Menu.Models
+
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public List<DishIngredient>? DishIngredients { get; set; }
+    }
+
+}
